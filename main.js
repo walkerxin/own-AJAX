54dc5fd198a52dd2a9e09de77c84246c253b5083
walkerxin <1464193658@qq.com>
2019-02-25 16:28:50 +0800

    升级符合 Promise 标准

--- main.js
+++ main.js
@@ -7,27 +7,40 @@ window.jQuery = function(nodeOrSelector) {
     return nodes
 }
 
-window.jQuery.ajax = function(url, method, body, success, fail) {
-    // ajax async javascript and xml
-    // jsonp
-    let request = new XMLHttpRequest()
-    request.open(method, url)
-    request.onreadystatechange = function() {
-        if(request.readyState === 4) {
-            if(request.status >= 200 && request.status < 300) {
-                success.call(undefined, request.responseText)
-            }else {
-                fail.call(undefined, request)
+window.jQuery.ajax = function({url, method, body, headers}) {
+    return new Promise(function(resolve, reject) {
+        let request = new XMLHttpRequest()
+        request.open(method, url)
+        for(let key in headers) {
+            request.setRequestHeader(key, headers[key])
+        }
+        request.onreadystatechange = function() {
+            if(request.readyState === 4) {
+                if(request.status >= 200 && request.status < 300) {
+                    resolve.call(undefined, request.responseText)
+                }else {
+                    reject.call(undefined, request)
+                }
             }
         }
-    }
-    request.send(body)
+        request.send(body)
+    })
 }
 
 myButton.addEventListener('click', (e) => {
-    jQuery.ajax('/xxx', 'post', undefined, (responseText) => {
-        console.log(responseText)
-    }, (request) => {
-        console.log('error', request.status)
-    })
+    jQuery.ajax({
+        url: 'http://localhost:9001/xxx', 
+        method: 'get',
+        headers: {
+            sdf: 'test1',
+            'Content-Type': 'x-www-form-urlencoded'
+        }
+    }).then(
+        (responseText) => {
+            console.log('获取成功', responseText)
+        },
+        (request) => {
+            console.log('获取失败', request)
+        }
+    )
 })
